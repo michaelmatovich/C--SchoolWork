b2863d525ccb47b731699396d5bbc0566dc8fa8d
michaelmatovich <michaelmatovich@ymail.com>
2022-06-17 02:14:46 -0400

    CRUDelicious added

---
+++ ORMs/CRUDelicious/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CRUDelicious.Models;
+
+namespace CRUDelicious.Controllers;
+
+public class HomeController : Controller
+{
+    private MyContext _context;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        ViewBag.AllDishes = _context.Dishes.Take(5).ToList();
+        return View();
+    }
+    [HttpGet("New")]
+    public IActionResult New()
+    {
+        return View();
+    }
+    [HttpPost("new/process")]
+    public IActionResult AddDish(Dish newDish)
+    {
+        if(ModelState.IsValid)
+        {
+            _context.Add(newDish);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+        else
+        {
+            return View("New");
+        }
+    }
+    [HttpPost("edit/process/{dishId}")]
+    public IActionResult UpdateDish(int dishId, Dish updatedDish)
+    {
+        Dish oldDish = _context.Dishes.FirstOrDefault(d => d.DishId == dishId);
+        oldDish.Name = updatedDish.Name;
+        oldDish.Chef = updatedDish.Chef;
+        oldDish.Tastiness = updatedDish.Tastiness;
+        oldDish.Calories = updatedDish.Calories;
+        oldDish.Description = updatedDish.Description;
+        oldDish.UpdatedAt = DateTime.Now;
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
+    [HttpGet("/dish/delete/{dishId}")]
+    public IActionResult DeleteDish(int dishId)
+    {
+        Dish dishToDelete = _context.Dishes.SingleOrDefault(d => d.DishId == dishId);
+        _context.Dishes.Remove(dishToDelete);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    [HttpGet("/edit/{dishId}")]
+    public IActionResult Edit(int dishId)
+    {
+        Dish dishToEdit = _context.Dishes.FirstOrDefault(d => d.DishId == dishId);
+
+        return View(dishToEdit);    
+    }
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ORMs/CRUDelicious/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using CRUDelicious.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+//db connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
